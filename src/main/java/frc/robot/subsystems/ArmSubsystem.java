d03a182301af5c01423643958007ae8418a365af
Deven Mahida <devenmahida08@gmail.com>
2023-10-02 15:28:22 -0700

    I did the assignment.

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,18 +1,28 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
   //add motors here
+  private TalonFX motor = new TalonFX(0);
+  
   public ArmSubsystem() {
-    //make proper initializations
   }
 
+  public void setPower(double power) {
+    motor.set(ControlMode.PercentOutput, power);
+  }
+  
   //consider what methods are necessary for the motor to run effectively
   
   @Override
   public void periodic() {
+
 
   }
 }
